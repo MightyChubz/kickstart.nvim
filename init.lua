81c778d86ce702e7357b59465d474a0b2b764312
Alexander Cusaac (MightyChubz) <theradcast16@gmail.com>
2026-05-31 00:09:45 -0400

    Change theme to modus

--- init.lua
+++ init.lua
@@ -398,18 +398,14 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
+  vim.pack.add { gh 'miikanissi/modus-themes.nvim' }
   ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
+  require('modus-themes').setup {
     styles = {
       comments = { italic = false }, -- Disable italics in comments
     },
   }
-
-  -- Load the colorscheme here.
-  -- Like many other themes, this one has different styles, and you could load
-  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'modus'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
